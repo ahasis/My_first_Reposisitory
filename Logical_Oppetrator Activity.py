cb384f36a6dc610debbdd34d02a290136a936ec0
ahasis <163393479+ahasis@users.noreply.github.com>
2024-05-27 18:42:01 +0600

    Add files via upload

---
+++ Logical_Oppetrator Activity.py
@@ -0,0 +1,7 @@
+x=int(input("Enter your age:"))
+y=input("Are you a student? (yes/no): ").lower()
+
+if x<=12 or ((x>=13 and x<=18) and y=="yes"):
+    print("You are eligible for a discount on the movie ticket.")
+else:
+    print("You are not eligible for a discount on the movie ticket.")
